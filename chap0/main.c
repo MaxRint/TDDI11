e023d49e92fcb2156d12eb7df8782017f97929c1
maxri670 <max@ringback.se>
2026-04-29 13:31:19 +0200

    started lab 2 :(((

---
+++ chap0/main.c
@@ -0,0 +1,11 @@
+#include <libepc.h>
+
+int main(int argc, char *argv[])
+{
+   ClearScreen(0x07);
+   SetCursorPosition(0, 0);
+   
+   PutString(">>>>>>> Maximus Wetterholm-Österman maxve266, Max 05a maxri670 <<<<<<<\r\n");
+   
+   return 0;
+}
