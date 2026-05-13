11cd7fa9b68ada0280e2753d2e2a1fc5c3a43a3d
maxri670 <max@ringback.se>
2026-05-13 16:17:13 +0200

    lab4 complete

---
+++ chap4/packet.c
@@ -0,0 +1,77 @@
+#include <os_cpu.h>
+#include <os_cfg.h>
+#include <ucos_ii.h> /* os_c??.h must be included before ! */
+#include <libepc.h>
+
+#include "inbound.h"
+#include "elapsed.h"
+#include "serial.h"
+
+static OS_EVENT *SendP_sem = 0;
+void ReceivePackets(void)
+{
+  SerialInit() ;
+  
+  for (;;)
+  {
+    int type, byte, bytes ;
+    BYTE8 *bfr ;
+    
+    if (SerialGet() != 0xFF)
+      continue ;
+    
+    switch (type = SerialGet())
+    {
+      default:
+        continue ;
+      case 1:
+      case 2:
+        break ;
+    }
+    
+    bytes = SerialGet();
+    bfr = (BYTE8 *) malloc(bytes) ;
+    if (!bfr)
+    {
+      for (;;)
+
+        ;
+    }
+    
+    for (byte = 0; byte < bytes; byte++)
+    {
+      bfr[byte] = SerialGet() ;
+    }
+    switch (type)
+    {
+      case 1:
+        PostText(bfr) ;
+        break ;
+      case 2:
+        PostTime(bfr) ;
+        break ;
+    }
+  }
+}
+
+void SendPacket(int type, BYTE8 *bfr, int bytes)
+{
+  INT8U *err = NULL; 
+
+
+  OSSemPend(SendP_sem, 0, err);
+
+
+
+  SerialPut(0xFF);
+  SerialPut(type);
+  SerialPut(bytes);
+
+  for(int i = 0; i < bytes; i++) {
+    char ch = bfr[i];
+    SerialPut(ch);
+  }
+
+  OSSemPost(SendP_sem);
+
+}
